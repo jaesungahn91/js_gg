0b8587f85adb3dc72dd44bb118dd0be841ea2080
ajs <sung31233123@gmail.com>
2020-06-26 13:26:51 +0900

    - summoner api 요청확인 - pom.xml google guava 추가 - js.properties 파일 추가

--- src/main/java/com/home/js_gg/api/HttpConnection.java
+++ src/main/java/com/home/js_gg/api/HttpConnection.java
@@ -2,11 +2,58 @@ package com.home.js_gg.api;
 
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
 @Component
 public class HttpConnection {
     //TODO : 리턴값을 상태메세지를 포함하는 도메인으로 만들어 전달
-    public String connectHttp(String url){
+    public String connectHttp(String _url, Map<String, Object> _params, String _method) throws IOException {
+        String result = "";
+        try {
+            // POST params 입출력
+            /*StringBuilder paramData = new StringBuilder();
+            if(_params != null){
+                for(Map.Entry<String, Object> param : _params.entrySet()){
+                    if(paramData.length() != 0) paramData.append("\n");
+                    paramData.append(param.getKey());
+                    paramData.append(":");
+                    paramData.append(param.getValue());
+                }
+            }
+            byte[] paramDataBytes = paramData.toString().getBytes("UTF-8");*/
 
-        return "";
+            URL url = new URL(_url);
+
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(5000);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.addRequestProperty("X-Riot-Token", _params.get("key").toString());
+            // POST params 입출력
+//            conn.setRequestProperty("Content-Length", String.valueOf(paramDataBytes.length));
+
+            conn.setDoOutput(false);
+
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                // POST params 입출력
+//                conn.getOutputStream().write(paramDataBytes);
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                String line;
+                while((line = br.readLine()) != null) {
+                    result = line;
+                }
+                br.close();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
     }
 }
--- src/main/java/com/home/js_gg/api/summoner/SummonerApi.java
+++ src/main/java/com/home/js_gg/api/summoner/SummonerApi.java
@@ -1,10 +1,18 @@
 package com.home.js_gg.api.summoner;
 
+import com.google.common.collect.Maps;
+import com.home.js_gg.api.HttpConnection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * summoner api
+ */
 @RestController
 @PropertySource("classpath:js.properties")
 @RequestMapping(value = "summoner")
@@ -13,9 +21,31 @@ public class SummonerApi {
     @Value("${api.basic.url}")
     private String basicUrl;
 
-    @RequestMapping(value = "byName")
-    public String getSummonersInfoByName(){
-        return basicUrl;
+    @Value("${api.key}")
+    private String key;
+
+    /**
+     * The Http connection.
+     */
+    @Autowired
+    HttpConnection httpConnection;
+
+    /**
+     * Gets summoners info by name.
+     *
+     * @param userName the user name
+     * @return the summoners info by name
+     */
+    @RequestMapping(value = "byName/{userName}", method = RequestMethod.GET)
+    public String getSummonersInfoByName(
+            @PathVariable(value = "userName", required = true) String userName
+    ) throws IOException {
+//        String url = basicUrl + "summoner/v4/summoners/by-name/" + userName + "?api_key=" + key;
+        String url = basicUrl + "summoner/v4/summoners/by-name/" + userName;
+        Map<String, Object> params = Maps.newHashMap();
+        params.put("key", key);
+        String data = httpConnection.connectHttp(url, params, "GET");
+        return data;
     }
 
 }
