c2d8ed78b0994465ceff3c9d6f34bf7d702d6d39
jaesungahn91 <jaesungahn91@gmail.com>
2020-08-18 17:55:28 +0900

    -

--- src/main/java/com/home/js_gg/controller/summoner/SummonerController.java
+++ src/main/java/com/home/js_gg/controller/summoner/SummonerController.java
@@ -1,8 +1,10 @@
 package com.home.js_gg.controller.summoner;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("")
 public class SummonerController {
 
 }
